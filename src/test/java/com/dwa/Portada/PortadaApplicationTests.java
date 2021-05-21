dc52d6814027ca7781601d4904cf604c430adc85
Javi <Javi@Smoky>
2021-05-20 21:05:02 -0500

    Mi primer comit

---
+++ src/test/java/com/dwa/Portada/PortadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.Portada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
